02a99c7a8e321a571e1cb9dbf3841093ffbcdd32
RagDo <rag.do200@gmail.com>
2026-02-09 13:19:42 +0300

    updating the db feilds

--- app_test/models.py
+++ app_test/models.py
@@ -5,7 +5,5 @@ class userProfile(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    age = models.IntegerField()
-    bio = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
