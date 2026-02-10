86da30578a121dbbeaf343a1c2c0444ddaf698f5
RagDo <rag.do200@gmail.com>
2026-02-10 14:41:03 +0300

    clean urls

--- app_test/urls.py
+++ app_test/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # APIView CRUD example
-    path('api/create_user', views.create_user, name='create_user'),
-    path('api/read_user', views.read_user, name='read_user'),
-    path('api/update_user', views.update_user, name='update_user'),
-    path('api/delete_user', views.delete_user, name='delete_user'),
-    path('api/all_users', views.all_users, name='all_users'),
+    path('api/create_user', views.create_user.as_view(), name='create_user'),
+    path('api/read_user/<int:user_id>/', views.read_user.as_view(), name='read_user'),
+    path('api/update_user/<int:user_id>/', views.update_user.as_view(), name='update_user'),
+    path('api/delete_user/<int:user_id>/', views.delete_user.as_view(), name='delete_user'),
+    path('api/all_users', views.all_users.as_view(), name='all_users'),
 
     # APIView example
     # path("api/add_task/<int:x>/<int:y>/", views.AddTask.as_view(), name="api_add_task"),
